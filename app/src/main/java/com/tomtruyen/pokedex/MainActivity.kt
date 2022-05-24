9a06528fdd576fffa582204970578c1f13a62e07
TomTruyen <tom.truyen@gmail.com>
2022-05-24 22:24:15 +0200

    feat: NavController + HomeScreen impl.

--- app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tomtruyen.pokedex.ui.screens.home.HomeScreen
 import com.tomtruyen.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
                 ) {
-                    Greeting("Android")
+                    composable("home") {
+                        HomeScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokedexTheme {
-        Greeting("Android")
     }
 }
