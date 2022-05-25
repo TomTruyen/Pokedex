5bd85c7ae46368c9284c1b8cda390fd625eac68b
TomTruyen <tom.truyen@gmail.com>
2022-05-25 09:06:22 +0200

    feat: Loader component

---
+++ app/src/main/java/com/tomtruyen/pokedex/ui/shared/components/Loader.kt
@@ -0,0 +1,23 @@
+package com.tomtruyen.pokedex.ui.shared.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun Loader() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(color = Color.Red)
+    }
+}
