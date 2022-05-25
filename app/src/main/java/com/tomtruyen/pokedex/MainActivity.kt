b0ed4b2572c8ae5da0755930aa63fa6d254e8367
TomTruyen <tom.truyen@gmail.com>
2022-05-25 21:30:49 +0200

    feat: set status bar color to white

--- app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
@@ -3,34 +3,41 @@ package com.tomtruyen.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.tomtruyen.pokedex.ui.screens.home.HomeScreen
 import com.tomtruyen.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
                 val navController = rememberNavController()
 
-                NavHost(
-                    navController = navController,
-                    startDestination = "home"
+                Surface(
+                    color = MaterialTheme.colors.background
                 ) {
-                    composable("home") {
-                        HomeScreen()
+                    val systemUiController = rememberSystemUiController()
+                    systemUiController.setStatusBarColor(MaterialTheme.colors.background)
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
+                        composable("home") {
+                            HomeScreen()
+                        }
                     }
                 }
+
             }
         }
     }
