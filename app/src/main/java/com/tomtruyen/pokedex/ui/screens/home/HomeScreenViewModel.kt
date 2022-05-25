31fa1620b16272a58402e722abf8b95360ae2217
TomTruyen <tom.truyen@gmail.com>
2022-05-25 16:27:25 +0200

    feat: search method

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/home/HomeScreenViewModel.kt
@@ -8,22 +8,32 @@ import com.tomtruyen.pokedex.service.PokemonApi
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel() : ViewModel() {
+    var _pokemon = listOf<Pokemon>()
     var pokemon = mutableStateOf<List<Pokemon>>(listOf())
     var error = mutableStateOf<String>("")
     var isLoading = mutableStateOf<Boolean>(true)
+    var searchQuery = mutableStateOf<String>("")
 
     init {
         viewModelScope.launch {
             isLoading.value = true
 
             try {
-                val result = PokemonApi.service.getAll()
-                pokemon.value = result
+                _pokemon = PokemonApi.service.getAll()
+                pokemon.value = _pokemon.map { it.copy() }
             } catch (e: Exception) {
                 error.value = e.message ?: "Something went wrong"
             }
 
             isLoading.value = false
+        }
+    }
+
+    fun search(value: String): Unit {
+        searchQuery.value = value
+
+        pokemon.value = _pokemon.filter {
+            it.id.toString().contains(value) || it.name.contains(value)
         }
     }
 }
