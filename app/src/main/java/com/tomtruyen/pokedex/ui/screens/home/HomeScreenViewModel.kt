a4f8340dd226ad9ed99372505c92f094f73aa307
TomTruyen <tom.truyen@gmail.com>
2022-05-25 21:31:19 +0200

    feat: sort method
    This handles the sorting of the pokemon according to the enum selected

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/home/HomeScreenViewModel.kt
@@ -3,6 +3,7 @@ package com.tomtruyen.pokedex.ui.screens.home
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tomtruyen.pokedex.enums.Sort
 import com.tomtruyen.pokedex.models.Pokemon
 import com.tomtruyen.pokedex.service.PokemonApi
 import kotlinx.coroutines.launch
@@ -13,6 +14,7 @@ class HomeScreenViewModel() : ViewModel() {
     var error = mutableStateOf<String>("")
     var isLoading = mutableStateOf<Boolean>(true)
     var searchQuery = mutableStateOf<String>("")
+    var sort = mutableStateOf<Sort>(Sort.NUMERIC_ASC)
 
     init {
         viewModelScope.launch {
@@ -29,11 +31,22 @@ class HomeScreenViewModel() : ViewModel() {
         }
     }
 
-    fun search(value: String): Unit {
+    fun search(value: String) {
         searchQuery.value = value
 
         pokemon.value = _pokemon.filter {
             it.id.toString().contains(value) || it.name.contains(value)
+        }
+    }
+
+    fun sort(newSort: Sort) {
+        sort.value = newSort
+
+        pokemon.value = when(newSort) {
+            Sort.ALPHABETIC_ASC -> _pokemon.sortedBy { pokemon -> pokemon.name }
+            Sort.ALPHABETIC_DESC -> _pokemon.sortedByDescending { pokemon -> pokemon.name }
+            Sort.NUMERIC_ASC -> _pokemon.sortedBy { pokemon -> pokemon.id }
+            Sort.NUMERIC_DESC -> _pokemon.sortedByDescending { pokemon -> pokemon.id }
         }
     }
 }
