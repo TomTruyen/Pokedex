b5fe5a6dc17ed3119ca62e87bfec40405253ad39
TomTruyen <tom.truyen@gmail.com>
2022-05-26 18:01:29 +0200

    feat: getter for background color of detail page

--- app/src/main/java/com/tomtruyen/pokedex/utils/PokemonUtils.kt
+++ app/src/main/java/com/tomtruyen/pokedex/utils/PokemonUtils.kt
@@ -27,5 +27,29 @@ class PokemonUtils {
                 else -> return R.color.black
             }
         }
+
+        fun getDetailBackgroundGradient(type: String): List<Color> {
+            return when (type.lowercase()) {
+                "grass" -> listOf(Color(0xFF7ECD8B), Color(0xFF89E2B3))
+                "bug" -> listOf(Color(0xFFB5C91C), Color(R.color.bug))
+                "fire" -> listOf(Color(0xFFE68061), Color(0xFFDBB469))
+                "water" -> listOf(Color(0xFF4DA4DB), Color(R.color.water))
+                "fighting" -> listOf(Color(0xFFDB352E), Color(R.color.fighting))
+                "normal" -> listOf(Color(R.color.normal), Color(0xFFBEc9CC))
+                "electric" -> listOf(Color(0xFFf7dc2c), Color(R.color.electric))
+                "psychic" -> listOf(Color(0xFFfa6e99), Color(R.color.psychic))
+                "poison" -> listOf(Color(0xFFd38fe3), Color(R.color.poison))
+                "ghost" -> listOf(Color(0xFF8c70ba), Color(R.color.ghost))
+                "rock" ->listOf(Color(0xFFd1b93d), Color(R.color.rock))
+                "ground" -> listOf(Color(0xFFf5ce6c), Color(R.color.ground))
+                "dark" -> listOf(Color(0xFF8a6b57), Color(R.color.dark))
+                "ice" -> listOf(Color(0xFFa4edea), Color(R.color.ice))
+                "flying" -> listOf(Color(0xFFb59ff5), Color(R.color.flying))
+                "dragon" -> listOf(Color(0xFF8150fa), Color(R.color.dragon))
+                "fairy" -> listOf(Color(0xFFf097c3), Color(R.color.fairy))
+                "steel" -> listOf(Color(0xFFcfcfe8), Color(R.color.steel))
+                else -> return listOf(Color(0xFFE5E5E5), Color.White)
+            }
+        }
     }
 }
