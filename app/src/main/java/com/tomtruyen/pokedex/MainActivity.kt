57522ca0bdd748dd1f45ccdf6ae9f88cf0376ad9
TomTruyen <tom.truyen@gmail.com>
2022-05-26 13:49:38 +0200

    feat: navigation animation

--- app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
@@ -3,24 +3,31 @@ package com.tomtruyen.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.navigation
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.tomtruyen.pokedex.ui.screens.Screens
+import com.tomtruyen.pokedex.ui.screens.detail.DetailScreen
+import com.tomtruyen.pokedex.ui.screens.detail.DetailScreenViewModel
 import com.tomtruyen.pokedex.ui.screens.home.HomeScreen
+import com.tomtruyen.pokedex.ui.screens.home.HomeScreenViewModel
 import com.tomtruyen.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterialApi::class)
+    @OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                val navController = rememberNavController()
+                val navController = rememberAnimatedNavController()
 
                 Surface(
                     color = MaterialTheme.colors.background
@@ -28,12 +35,44 @@ class MainActivity : ComponentActivity() {
                     val systemUiController = rememberSystemUiController()
                     systemUiController.setStatusBarColor(MaterialTheme.colors.background)
 
-                    NavHost(
+                    AnimatedNavHost(
                         navController = navController,
-                        startDestination = "home"
+                        startDestination = Screens.Home.route
                     ) {
-                        composable("home") {
-                            HomeScreen()
+                        composable(
+                            route = Screens.Home.route,
+                            exitTransition = { ->
+                                slideOutHorizontally(
+                                    targetOffsetX = { -300 },
+                                    animationSpec = tween(300)
+                                ) + fadeOut(animationSpec = tween(300))
+                            },
+                            popEnterTransition = { ->
+                                slideInHorizontally(
+                                    initialOffsetX = { 300 },
+                                    animationSpec = tween(300)
+                                ) + fadeIn(animationSpec = tween(300))
+                            }
+                        ) {
+                            HomeScreen(navController)
+                        }
+
+                        composable(
+                            route = Screens.Details.route,
+                            enterTransition = { ->
+                                slideInHorizontally(
+                                    initialOffsetX = { 300 },
+                                    animationSpec = tween(300)
+                                ) + fadeIn(animationSpec = tween(300))
+                            },
+                            popExitTransition = { ->
+                                slideOutHorizontally(
+                                    targetOffsetX = { -300 },
+                                    animationSpec = tween(300)
+                                ) + fadeOut(animationSpec = tween(300))
+                            }
+                        ) {
+                            DetailScreen(navController, DetailScreenViewModel(it.arguments?.getString("pokemonId")?.toInt()))
                         }
                     }
                 }
