7ac2f5edc1116ae92da95b689199fe3e76e0c0e1
TomTruyen <tom.truyen@gmail.com>
2022-05-26 18:03:13 +0200

    feat: implement About,  Statitistics and carousel

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
@@ -1,23 +1,42 @@
 package com.tomtruyen.pokedex.ui.screens.detail
 
+import android.graphics.Paint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.tomtruyen.pokedex.R
-import com.tomtruyen.pokedex.ui.shared.components.Error
-import com.tomtruyen.pokedex.ui.shared.components.Loader
-import kotlinx.coroutines.launch
+import com.tomtruyen.pokedex.ui.shared.components.*
+import com.tomtruyen.pokedex.utils.PokemonUtils
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @Composable
 fun DetailScreen(navController: NavController, viewModel: DetailScreenViewModel) {
+    val toolbarScaffoldState = rememberCollapsingToolbarScaffoldState()
+
     val pokemon by remember { viewModel.pokemon }
     val isLoading by remember { viewModel.isLoading }
     val error by remember { viewModel.error }
@@ -27,38 +46,80 @@ fun DetailScreen(navController: NavController, viewModel: DetailScreenViewModel)
     } else if (error.isNotEmpty()) {
         Error(error = error)
     } else {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    backgroundColor = MaterialTheme.colors.background,
-                    elevation = 0.dp,
-                    title = { },
-                    navigationIcon = {
-                        IconButton(
-                            onClick = { navController.popBackStack() }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.ArrowBack,
-                                contentDescription = null
-                            )
-                        }
-                    },
-                    actions = {
-                        IconButton(
-                            onClick = {
-                                // TODO
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.Favorite,
-                                contentDescription = null,
-                            )
-                        }
+        pokemon?.let {
+            CollapsingToolbarScaffold(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = PokemonUtils.getDetailBackgroundGradient(
+                                it.types.first().type["name"] ?: ""
+                            ),
+                        )
+                    ),
+                state = toolbarScaffoldState,
+                scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+                toolbar = {
+                    // Calculate the textSize based on the current state of the toolbar
+                    val textSize = (17 + (34 - 12) * toolbarScaffoldState.toolbarState.progress).sp
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(125.dp)
+                            .pin()
+                            .background(color = Color.Transparent)
+                    )
+
+                    IconButton(
+                        modifier = Modifier.road(whenCollapsed = Alignment.TopStart, whenExpanded = Alignment.TopStart),
+                        onClick = { navController.popBackStack() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
                     }
-                )
+
+                    Text(
+                        text = it.name.replaceFirstChar { it.uppercase() },
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = textSize,
+                            color = Color.White
+                        ),
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .road(
+                                whenCollapsed = Alignment.TopCenter,
+                                whenExpanded = Alignment.BottomStart
+                            )
+                    )
+
+                    IconButton(
+                        modifier = Modifier.road(whenCollapsed = Alignment.TopEnd, whenExpanded = Alignment.TopEnd),
+                        onClick = {   }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.FavoriteBorder,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .padding(20.dp)
+                ) {
+                    ImageCarousel(pokemon = it)
+                    AboutCard(pokemon = it)
+                    StatisticsCard(pokemon = it)
+                }
             }
-        ) {
-            pokemon?.let { it1 -> Text(text = it1.name) }
         }
     }
 }
