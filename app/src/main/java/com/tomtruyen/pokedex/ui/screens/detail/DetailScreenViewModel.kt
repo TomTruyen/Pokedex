0b02c330ef66fa40f920f31931be126ff31312a9
TomTruyen <tom.truyen@gmail.com>
2022-05-26 20:08:54 +0200

    feat: get moves logic

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreenViewModel.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreenViewModel.kt
@@ -5,13 +5,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tomtruyen.pokedex.models.Pokemon
 import com.tomtruyen.pokedex.models.PokemonDetails
+import com.tomtruyen.pokedex.models.PokemonMove
 import com.tomtruyen.pokedex.service.PokemonApi
+import com.tomtruyen.pokedex.utils.PokemonUtils
 import kotlinx.coroutines.launch
 
 class DetailScreenViewModel(private val id: Int?): ViewModel() {
     var pokemon = mutableStateOf<PokemonDetails?>(null);
     var error = mutableStateOf<String>("")
     var isLoading = mutableStateOf<Boolean>(true)
+    var moves = mutableStateOf<List<PokemonMove>>(listOf())
 
     init {
         viewModelScope.launch {
@@ -19,6 +22,10 @@ class DetailScreenViewModel(private val id: Int?): ViewModel() {
                 if(id == null) throw Exception()
 
                 pokemon.value = PokemonApi.service.getById(id)
+
+                if(pokemon.value != null) {
+                    moves.value = PokemonUtils.getLevelUpMoves(pokemon.value!!.moves)
+                }
             } catch (e: Exception) {
                 error.value = e.message ?: "Something went wrong"
             }
