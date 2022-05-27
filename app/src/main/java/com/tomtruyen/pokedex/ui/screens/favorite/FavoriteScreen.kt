023dad9a1857284def244418ba636f8f6a4cc0b0
TomTruyen <tom.truyen@gmail.com>
2022-05-27 21:00:24 +0200

    feat: FavoriteScreen

---
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/favorite/FavoriteScreen.kt
@@ -0,0 +1,69 @@
+package com.tomtruyen.pokedex.ui.screens.favorite
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.tomtruyen.pokedex.ui.shared.components.PokedexItem
+import com.tomtruyen.pokedex.utils.viewModelFactory
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
+import org.koin.androidx.compose.get
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+
+@Composable
+fun FavoriteScreen(navController: NavHostController) {
+    val viewModel: FavoriteScreenViewModel = viewModel(factory = viewModelFactory {
+        FavoriteScreenViewModel(dao = get())
+    })
+
+    val toolbarScaffoldState = rememberCollapsingToolbarScaffoldState()
+
+    val pokemon by remember { viewModel.pokemon }
+
+    CollapsingToolbarScaffold(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(101, 203, 154),
+                        Color(21, 208, 220),
+                    ),
+                )
+            ),
+        state = toolbarScaffoldState,
+        scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+        toolbar = {
+            // Calculate the textSize based on the current state of the toolbar
+            val textSize =
+                (20 + (34 - 12) * toolbarScaffoldState.toolbarState.progress).sp
+
+            FavoriteToolbar(
+                textSize = textSize,
+                navController = navController,
+            )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            itemsIndexed(items = pokemon) { _, entry ->
+                PokedexItem(pokemon = entry, navController = navController)
+            }
+        }
+    }
+}
