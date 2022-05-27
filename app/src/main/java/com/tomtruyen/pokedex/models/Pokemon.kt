024c13718fe42f4d3840bd81c7f3667369e5006f
TomTruyen <tom.truyen@gmail.com>
2022-05-27 15:29:04 +0200

    feat: Entity annotations for room

--- app/src/main/java/com/tomtruyen/pokedex/models/Pokemon.kt
+++ app/src/main/java/com/tomtruyen/pokedex/models/Pokemon.kt
@@ -1,6 +1,18 @@
 package com.tomtruyen.pokedex.models
 
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.tomtruyen.pokedex.utils.converters.ListPokemonTypeConverter
+import com.tomtruyen.pokedex.utils.converters.MapStringTypeConverter
+
+
+@Entity
+@TypeConverters(ListPokemonTypeConverter::class, MapStringTypeConverter::class)
 data class Pokemon(
+    @PrimaryKey
+    @NonNull
     val id: Int,
     val name: String,
     val sprites: Map<String, String>,
--- app/src/main/java/com/tomtruyen/pokedex/models/PokemonDetails.kt
+++ app/src/main/java/com/tomtruyen/pokedex/models/PokemonDetails.kt
@@ -1,6 +1,22 @@
 package com.tomtruyen.pokedex.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.tomtruyen.pokedex.utils.converters.*
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "pokemon_details")
+@TypeConverters(
+    ListPokemonTypeConverter::class,
+    ListPokemonAbilityConverter::class,
+    ListPokemonStatisticConverter::class,
+    ListPokemonMoveConverter::class,
+    MapStringAnyTypeConverter::class
+)
 data class PokemonDetails(
+    @PrimaryKey
+    @NotNull
     val id: Int,
     val name: String,
     val abilities: List<PokemonAbility>,
