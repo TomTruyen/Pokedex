716e5de26a711d70999f822d02ec5205f3b4ad06
TomTruyen <tom.truyen@gmail.com>
2022-05-27 15:33:42 +0200

    feat: offline support

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreenViewModel.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreenViewModel.kt
@@ -1,19 +1,28 @@
 package com.tomtruyen.pokedex.ui.screens.detail
 
+import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tomtruyen.pokedex.database.dao.PokemonDao
+import com.tomtruyen.pokedex.database.dao.PokemonDetailsDao
 import com.tomtruyen.pokedex.models.Pokemon
 import com.tomtruyen.pokedex.models.PokemonDetails
 import com.tomtruyen.pokedex.models.PokemonMove
 import com.tomtruyen.pokedex.service.PokemonApi
+import com.tomtruyen.pokedex.utils.NetworkUtils
 import com.tomtruyen.pokedex.utils.PokemonUtils
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
-class DetailScreenViewModel(private val id: Int?): ViewModel() {
+class DetailScreenViewModel(
+    private val context: Context,
+    private val id: Int?,
+    private val dao: PokemonDetailsDao
+): ViewModel() {
     var pokemon = mutableStateOf<PokemonDetails?>(null);
-    var error = mutableStateOf<String>("")
-    var isLoading = mutableStateOf<Boolean>(true)
+    var error = mutableStateOf("")
+    var isLoading = mutableStateOf(true)
     var moves = mutableStateOf<List<PokemonMove>>(listOf())
 
     init {
@@ -21,7 +30,20 @@ class DetailScreenViewModel(private val id: Int?): ViewModel() {
             try {
                 if(id == null) throw Exception()
 
-                pokemon.value = PokemonApi.service.getById(id)
+                if(NetworkUtils.hasInternetConnection(context)) {
+                    pokemon.value = PokemonApi.service.getById(id)
+
+                    if(pokemon.value != null) {
+                        coroutineScope {
+                            dao.save(pokemon.value!!)
+                        }
+                    }
+                } else {
+                    pokemon.value = dao.getById(id)
+                    if(pokemon.value == null) {
+                        throw Exception("Couldn't find local data. Please try connecting to the internet.")
+                    }
+                }
 
                 if(pokemon.value != null) {
                     moves.value = PokemonUtils.getLevelUpMoves(pokemon.value!!.moves)
