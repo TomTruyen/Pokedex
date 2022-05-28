8cbcb4fbe54c04e8edffb68493c4f984be9ec6f8
TomTruyen <tom.truyen@gmail.com>
2022-05-28 20:54:41 +0200

    refactor: use unused composable

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
@@ -57,7 +57,7 @@ fun DetailScreen(navController: NavHostController, id: Int, modifier: Modifier =
         }
     }
 
-    val toolbarScaffoldState = rememberCollapsingToolbarScaffoldState()
+
 
     val pokemon by remember { viewModel.pokemon }
     val error by remember { viewModel.error }
@@ -97,114 +97,11 @@ fun DetailScreen(navController: NavHostController, id: Int, modifier: Modifier =
                 }
             } else {
                 pokemon?.let { pokemon ->
-
-                    val isFavorite by remember { viewModel.isFavorite }
-                    val isTeam by remember { viewModel.isTeam }
-                    val teamCount by remember { viewModel.teamCount }
-                    val moves by remember { viewModel.moves }
-
-                    Box {
-                        CollapsingToolbarScaffold(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .background(
-                                    brush = Brush.verticalGradient(
-                                        colors = PokemonUtils.getDetailBackgroundGradient(
-                                            pokemon.types.first().type["name"] ?: ""
-                                        ),
-                                    )
-                                ),
-                            state = toolbarScaffoldState,
-                            scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
-                            toolbar = {
-                                // Calculate the textSize based on the current state of the toolbar
-                                val textSize =
-                                    (20 + (34 - 12) * toolbarScaffoldState.toolbarState.progress).sp
-
-                                DetailToolbar(
-                                    pokemon = pokemon,
-                                    textSize = textSize,
-                                    navController = navController,
-                                    isFavorite = isFavorite,
-                                    onFavorite = {
-                                        viewModel.toggleFavorite()
-                                    }
-                                )
-                            }
-                        ) {
-                            BoxWithConstraints(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .fillMaxHeight()
-                            ) {
-                                Column(
-                                    modifier = Modifier
-                                        .verticalScroll(rememberScrollState())
-                                        .padding(20.dp)
-                                ) {
-                                    if(this@BoxWithConstraints.maxWidth < 600.dp) {
-                                        ImageCarousel(pokemon = pokemon)
-                                        AboutCard(pokemon = pokemon)
-                                        StatisticsCard(pokemon = pokemon)
-                                        MovesCard(moves = moves)
-                                        Evolutions(navController = navController, pokemon = pokemon)
-                                    } else {
-                                        Row(
-                                            modifier = Modifier
-                                                .padding(20.dp)
-                                        ) {
-                                            Column(
-                                                modifier = Modifier.weight(1f)
-                                            ) {
-                                                ImageCarousel(pokemon = pokemon)
-                                                AboutCard(pokemon = pokemon)
-                                            }
-
-                                            Spacer(modifier = Modifier.width(32.dp))
-
-                                            Column(
-                                                modifier = Modifier.weight(1f)
-                                            ) {
-                                                MovesCard(moves = moves)
-                                                Evolutions(navController = navController, pokemon = pokemon)
-                                            }
-                                        }
-
-                                        StatisticsCard(
-                                            pokemon = pokemon,
-                                            modifier = Modifier.padding(horizontal = 16.dp)
-                                        )
-                                    }
-
-                                    Spacer(modifier = Modifier.height(50.dp))
-                                }
-                            }
-                        }
-
-                        var buttonText = "Toevoegen aan mijn team"
-
-                        if(isTeam) {
-                            buttonText = "Verwijderen uit mijn team"
-                        } else if (teamCount >= 6) {
-                            buttonText = "Team is vol"
-                        }
-
-                        PrimaryButton(
-                            text = buttonText,
-                            enabled = isTeam || teamCount < 6,
-                            onClick = {
-                                if(isTeam) {
-                                    viewModel.removeFromTeam()
-                                } else {
-                                    viewModel.addToTeam()
-                                }
-                            },
-                            modifier = Modifier
-                                .align(Alignment.BottomCenter)
-                                .padding(16.dp)
-                        )
-                    }
+                    DetailScreenContent(
+                        pokemon = pokemon,
+                        navController = navController,
+                        viewModel = viewModel
+                    )
                 }
             }
         }
@@ -212,8 +109,8 @@ fun DetailScreen(navController: NavHostController, id: Int, modifier: Modifier =
 }
 
 @Composable
-private fun DetailScreenContent(toolbarScaffoldState: CollapsingToolbarScaffoldState, pokemon: PokemonDetails, navController: NavHostController, viewModel: DetailScreenViewModel) {
-
+private fun DetailScreenContent(pokemon: PokemonDetails, navController: NavHostController, viewModel: DetailScreenViewModel) {
+    val toolbarScaffoldState = rememberCollapsingToolbarScaffoldState()
 
     val isFavorite by remember { viewModel.isFavorite }
     val isTeam by remember { viewModel.isTeam }
