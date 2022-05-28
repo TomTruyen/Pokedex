f96c225e6c32ddc98331207b3d86394b1cb220cc
TomTruyen <tom.truyen@gmail.com>
2022-05-28 15:33:16 +0200

    feat: add Team button

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/detail/DetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -34,10 +35,11 @@ fun DetailScreen(navController: NavHostController, id: Int?) {
     val viewModel: DetailScreenViewModel = viewModel(factory = viewModelFactory {
         DetailScreenViewModel(
             context = LocalContext.current,
-            id = id,
+            id = id ?: -1,
             dao = get(),
             pokemonDao = get(),
-            favoritePokemonDao = get()
+            favoritePokemonDao = get(),
+            teamPokemonDao = get()
         )
     })
 
@@ -48,8 +50,9 @@ fun DetailScreen(navController: NavHostController, id: Int?) {
     val isLoading by remember { viewModel.isLoading }
     val isRefreshing by remember { viewModel.isRefreshing }
     val isFavorite by remember { viewModel.isFavorite }
+    val isTeam by remember { viewModel.isTeam }
+    val teamCount by remember { viewModel.teamCount }
     val moves by remember { viewModel.moves }
-
 
     if(isLoading) {
         Loader()
@@ -83,47 +86,72 @@ fun DetailScreen(navController: NavHostController, id: Int?) {
                 }
             } else {
                 pokemon?.let { it ->
-                    CollapsingToolbarScaffold(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .background(
-                                brush = Brush.verticalGradient(
-                                    colors = PokemonUtils.getDetailBackgroundGradient(
-                                        it.types.first().type["name"] ?: ""
-                                    ),
-                                )
-                            ),
-                        state = toolbarScaffoldState,
-                        scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
-                        toolbar = {
-                            // Calculate the textSize based on the current state of the toolbar
-                            val textSize =
-                                (20 + (34 - 12) * toolbarScaffoldState.toolbarState.progress).sp
-
-                            DetailToolbar(
-                                pokemon = it,
-                                textSize = textSize,
-                                navController = navController,
-                                isFavorite = isFavorite,
-                                onFavorite = {
-                                    viewModel.toggleFavorite()
-                                }
-                            )
-                        }
-                    ) {
-
-                        Column(
+                    Box {
+                        CollapsingToolbarScaffold(
                             modifier = Modifier
-                                .verticalScroll(rememberScrollState())
-                                .padding(20.dp)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .background(
+                                    brush = Brush.verticalGradient(
+                                        colors = PokemonUtils.getDetailBackgroundGradient(
+                                            it.types.first().type["name"] ?: ""
+                                        ),
+                                    )
+                                ),
+                            state = toolbarScaffoldState,
+                            scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+                            toolbar = {
+                                // Calculate the textSize based on the current state of the toolbar
+                                val textSize =
+                                    (20 + (34 - 12) * toolbarScaffoldState.toolbarState.progress).sp
+
+                                DetailToolbar(
+                                    pokemon = it,
+                                    textSize = textSize,
+                                    navController = navController,
+                                    isFavorite = isFavorite,
+                                    onFavorite = {
+                                        viewModel.toggleFavorite()
+                                    }
+                                )
+                            }
                         ) {
-                            ImageCarousel(pokemon = it)
-                            AboutCard(pokemon = it)
-                            StatisticsCard(pokemon = it)
-                            MovesCard(moves = moves)
-                            Evolutions(navController = navController, pokemon = it)
+                            Column(
+                                modifier = Modifier
+                                    .verticalScroll(rememberScrollState())
+                                    .padding(20.dp)
+                            ) {
+                                    ImageCarousel(pokemon = it)
+                                    AboutCard(pokemon = it)
+                                    StatisticsCard(pokemon = it)
+                                    MovesCard(moves = moves)
+                                    Evolutions(navController = navController, pokemon = it)
+                                    Spacer(modifier = Modifier.height(50.dp))
+                            }
                         }
+
+                        var buttonText = "Toevoegen aan mijn team"
+
+                        if(isTeam) {
+                            buttonText = "Verwijderen uit mijn team"
+                        } else if (teamCount >= 6) {
+                            buttonText = "Team is vol"
+                        }
+
+                        PrimaryButton(
+                            text = buttonText,
+                            enabled = isTeam || teamCount < 6,
+                            onClick = {
+                                if(isTeam) {
+                                    viewModel.removeFromTeam()
+                                } else {
+                                    viewModel.addToTeam()
+                                }
+                            },
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .padding(16.dp)
+                        )
                     }
                 }
             }
