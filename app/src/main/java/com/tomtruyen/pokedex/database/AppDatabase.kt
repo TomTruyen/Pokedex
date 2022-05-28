4b8200903853fca32685944c607af78b79c7e318
TomTruyen <tom.truyen@gmail.com>
2022-05-28 15:32:16 +0200

    fix: missing TeamPokemon entity in database

--- app/src/main/java/com/tomtruyen/pokedex/database/AppDatabase.kt
+++ app/src/main/java/com/tomtruyen/pokedex/database/AppDatabase.kt
@@ -6,12 +6,14 @@ import androidx.room.RoomDatabase
 import com.tomtruyen.pokedex.database.dao.FavoritePokemonDao
 import com.tomtruyen.pokedex.database.dao.PokemonDao
 import com.tomtruyen.pokedex.database.dao.PokemonDetailsDao
+import com.tomtruyen.pokedex.database.dao.TeamPokemonDao
 import com.tomtruyen.pokedex.models.FavoritePokemon
 import com.tomtruyen.pokedex.models.Pokemon
 import com.tomtruyen.pokedex.models.PokemonDetails
+import com.tomtruyen.pokedex.models.TeamPokemon
 
 @Database(
-    entities = [Pokemon::class, PokemonDetails::class, FavoritePokemon::class],
+    entities = [Pokemon::class, PokemonDetails::class, FavoritePokemon::class, TeamPokemon::class],
     version = 1,
     exportSchema = false,
 )
@@ -19,4 +21,5 @@ abstract class AppDatabase: RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
     abstract fun pokemonDetailsDao(): PokemonDetailsDao
     abstract fun favoritePokemonDao(): FavoritePokemonDao
+    abstract fun teamPokemonDao(): TeamPokemonDao
 }
