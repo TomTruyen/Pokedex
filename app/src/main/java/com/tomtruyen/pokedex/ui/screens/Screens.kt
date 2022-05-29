3f24a706a7d54ce20fc79733a05fea3ba3cfee39
TomTruyen <tom.truyen@gmail.com>
2022-05-29 14:23:32 +0200

    feat: getDeepLinks method

--- app/src/main/java/com/tomtruyen/pokedex/ui/screens/Screens.kt
+++ app/src/main/java/com/tomtruyen/pokedex/ui/screens/Screens.kt
@@ -1,8 +1,11 @@
 package com.tomtruyen.pokedex.ui.screens
 
+import androidx.navigation.NavDeepLink
+import androidx.navigation.navDeepLink
+
 sealed class Screens(val route: String) {
     object Home : Screens("home")
-    object Details : Screens("details/{pokemonId}")
+    object Details : Screens("details/{pokemon}")
     object Favorites : Screens("favorites")
     object Team : Screens("team")
 }
@@ -10,7 +13,11 @@ sealed class Screens(val route: String) {
 class ScreenUtils {
     companion object {
         fun toDetail(pokemonId: Int): String {
-            return Screens.Details.route.replace("{pokemonId}", pokemonId.toString())
+            return Screens.Details.route.replace("{pokemon}", pokemonId.toString())
+        }
+
+        fun getDeepLinks(): List<NavDeepLink> {
+            return listOf(navDeepLink { uriPattern = "pokedex://details/{pokemon}" })
         }
     }
 }
