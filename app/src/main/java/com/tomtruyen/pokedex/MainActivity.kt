cd11fbb860615dbdb06b2c83173e0de19c758b88
TomTruyen <tom.truyen@gmail.com>
2022-05-29 14:24:11 +0200

    fix: conditional deeplink
    This makes it possible to deeplink correctly depending on the screen. If it is a large screen the deeplink will not open a new screen for the details.

--- app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/pokedex/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -17,12 +22,16 @@ import com.tomtruyen.pokedex.ui.screens.favorite.FavoriteScreen
 import com.tomtruyen.pokedex.ui.screens.home.HomeScreen
 import com.tomtruyen.pokedex.ui.screens.team.TeamScreen
 import com.tomtruyen.pokedex.ui.theme.PokedexTheme
+import com.tomtruyen.pokedex.R
+import com.tomtruyen.pokedex.ui.screens.ScreenUtils
 
 class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val largeScreenSize = applicationContext.resources.getInteger(R.integer.large_screen_size)
 
         setContent {
             PokedexTheme {
@@ -31,91 +40,105 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    AnimatedNavHost(
-                        navController = navController,
-                        startDestination = Screens.Home.route
-                    ) {
-                        composable(
-                            route = Screens.Home.route,
-                            content = {
-                                HomeScreen(navController = navController)
-                            },
-                            exitTransition = { ->
-                                slideOutHorizontally(
-                                    targetOffsetX = { -300 },
-                                    animationSpec = tween(300)
-                                ) + fadeOut(animationSpec = tween(300))
-                            },
-                            popEnterTransition = { ->
-                                slideInHorizontally(
-                                    initialOffsetX = { 300 },
-                                    animationSpec = tween(300)
-                                ) + fadeIn(animationSpec = tween(300))
-                            }
-                        )
+                    BoxWithConstraints {
+                        val isLargeScreen = maxWidth >= largeScreenSize.dp
 
-                        composable(
-                            route = Screens.Details.route,
-                            enterTransition = { ->
-                                slideInHorizontally(
-                                    initialOffsetX = { 300 },
-                                    animationSpec = tween(300)
-                                ) + fadeIn(animationSpec = tween(300))
-                            },
-                            popExitTransition = { ->
-                                slideOutHorizontally(
-                                    targetOffsetX = { -300 },
-                                    animationSpec = tween(300)
-                                ) + fadeOut(animationSpec = tween(300))
-                            },
-                            content = {
-                                DetailScreen(
-                                    navController,
-                                    it.arguments?.getString("pokemonId")?.toInt() ?: -1
-                                )
-                            }
-                        )
+                        AnimatedNavHost(
+                            navController = navController,
+                            startDestination = Screens.Home.route
+                        ) {
+                            composable(
+                                route = Screens.Home.route,
+                                arguments = listOf(navArgument("pokemon") {
+                                    type = NavType.StringType
+                                }),
+                                deepLinks = if(isLargeScreen) ScreenUtils.getDeepLinks() else listOf(),
+                                content = {
+                                    HomeScreen(
+                                        navController = navController,
+                                        it.arguments?.getString("pokemon")
+                                    )
+                                },
+                                exitTransition = { ->
+                                    slideOutHorizontally(
+                                        targetOffsetX = { -300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeOut(animationSpec = tween(300))
+                                },
+                                popEnterTransition = { ->
+                                    slideInHorizontally(
+                                        initialOffsetX = { 300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeIn(animationSpec = tween(300))
+                                }
+                            )
 
-                        composable(
-                            route = Screens.Favorites.route,
-                            enterTransition = { ->
-                                slideInHorizontally(
-                                    initialOffsetX = { 300 },
-                                    animationSpec = tween(300)
-                                ) + fadeIn(animationSpec = tween(300))
-                            },
-                            popExitTransition = { ->
-                                slideOutHorizontally(
-                                    targetOffsetX = { -300 },
-                                    animationSpec = tween(300)
-                                ) + fadeOut(animationSpec = tween(300))
-                            },
-                            content = {
-                                FavoriteScreen(navController)
-                            }
-                        )
+                            composable(
+                                route = Screens.Details.route,
+                                arguments = listOf(navArgument("pokemon") {
+                                    type = NavType.StringType
+                                }),
+                                deepLinks = if(!isLargeScreen) ScreenUtils.getDeepLinks() else listOf(),
+                                enterTransition = { ->
+                                    slideInHorizontally(
+                                        initialOffsetX = { 300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeIn(animationSpec = tween(300))
+                                },
+                                popExitTransition = { ->
+                                    slideOutHorizontally(
+                                        targetOffsetX = { -300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeOut(animationSpec = tween(300))
+                                },
+                                content = {
+                                    DetailScreen(
+                                        navController,
+                                        it.arguments?.getString("pokemon")
+                                    )
+                                }
+                            )
 
-                        composable(
-                            route = Screens.Team.route,
-                            enterTransition = { ->
-                                slideInHorizontally(
-                                    initialOffsetX = { 300 },
-                                    animationSpec = tween(300)
-                                ) + fadeIn(animationSpec = tween(300))
-                            },
-                            popExitTransition = { ->
-                                slideOutHorizontally(
-                                    targetOffsetX = { -300 },
-                                    animationSpec = tween(300)
-                                ) + fadeOut(animationSpec = tween(300))
-                            },
-                            content = {
-                                TeamScreen(navController)
-                            }
-                        )
+                            composable(
+                                route = Screens.Favorites.route,
+                                enterTransition = { ->
+                                    slideInHorizontally(
+                                        initialOffsetX = { 300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeIn(animationSpec = tween(300))
+                                },
+                                popExitTransition = { ->
+                                    slideOutHorizontally(
+                                        targetOffsetX = { -300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeOut(animationSpec = tween(300))
+                                },
+                                content = {
+                                    FavoriteScreen(navController)
+                                }
+                            )
+
+                            composable(
+                                route = Screens.Team.route,
+                                enterTransition = { ->
+                                    slideInHorizontally(
+                                        initialOffsetX = { 300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeIn(animationSpec = tween(300))
+                                },
+                                popExitTransition = { ->
+                                    slideOutHorizontally(
+                                        targetOffsetX = { -300 },
+                                        animationSpec = tween(300)
+                                    ) + fadeOut(animationSpec = tween(300))
+                                },
+                                content = {
+                                    TeamScreen(navController)
+                                }
+                            )
+                        }
                     }
                 }
-
             }
         }
     }
